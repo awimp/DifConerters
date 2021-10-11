b8e02286cdfbc15cb4d6900dc45d96b8828974c7
awimp <91920959+awimp@users.noreply.github.com>
2021-10-12 01:51:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Task1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first side: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second side: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter third side: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+
+            //var eqTr = new EquilTriangle(3);
+            var triangle = new Triangle(a,b,c);
+            //Console.WriteLine(eqTr.Perimeter());
+            Console.WriteLine("\nPerimeter of this triangle is " + triangle.Perimeter() +"\n");
+
+            Console.WriteLine("If you want to calculate angles type 'y'\n");
+
+            char examination = Convert.ToChar(Console.ReadLine());
+
+
+            if (examination == 'y')
+            {
+                Console.WriteLine("The angle A is " + triangle.AngleA() + "\n");
+                
+
+                Console.WriteLine("The angle B is " + triangle.AngleB() + "\n");
+                
+
+                Console.WriteLine("The angle C is " + triangle.AngleB() + "\n");
+               
+            }
+
+            
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace Task1
+{
+    class Triangle
+
+    {
+
+        public Triangle(double a)
+
+        {
+            this.a = a;
+            this.b = a;
+            this.c = a;
+        }
+
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        
+
+        public double Perimeter()
+        {
+            return (a+b+c);
+        }
+
+        
+        public double AngleA()
+        {
+
+            double A = (Math.Acos((((-a) * a) + (b * b) + (c * c)) / (2 * b * c)))*57.29577951308232;
+            return  A;            
+
+        }
+
+        public double AngleB()
+        {
+            double B = (Math.Acos((((-b) * b) + (a * a) + (c * c)) / (2 * a * c))) * 57.29577951308232;
+            return B;
+        }
+
+
+        public double AngleC()
+        {
+           
+            double C = (Math.Acos((((-c) * c) + (b * b) + (a * a)) / (2 * b * a))) * 57.29577951308232;
+            return C;
+        }
+
+        public void setSide(UInt16 length, double newSide)
+        {
+            if (length == 1) this.a = newSide;
+            else if (length == 2) this.b = newSide;
+            else if (length == 3) this.c = newSide;
+            else throw new Exception("Invalid side number:(");
+        }
+
+        public double a;
+        public double b;
+        public double c;
+
+    }
+
+    class EquilTriangle : Triangle
+
+
+    {
+        public EquilTriangle(double a) : base(a)
+
+        {
+            area = (a * a * Math.Sqrt(3)) / 4;
+        }
+
+        public double Area()
+        {
+            return area;
+        }
+
+        private double area;
+    }
+}
